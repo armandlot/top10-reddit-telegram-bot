792e4c62d8ec82ee2ddf301b59df9324b5cd0fcd
Armand Lottmann <armand.lottmann@koezio.co>
2022-08-09 16:24:30 +0200

    Bot is live

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ import config
 
 TOKEN = config.API_TOKEN
 
-updater.dispatcher.add_handler(CommandHandler("commande", fonction))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, pas_compris))
-
-updater.start_polling()
-updater.idle()
-
 def start(update, context):
     update.message.reply_text("""
 Bienvenue sur le bot officiel de tcollart.
@@ -25,7 +19,7 @@ def fonction(update, context):
     update.message.reply_text("Un message à répondre en fonction de la commande saisie par l'utilisateur")
 
 def pas_compris(update, context):
-    update.message.reply_text('Je n\'ai pas compris votre message "', update.message.text, "')
+    update.message.reply_text('Je n\'ai pas compris votre message', update.message.text )
 
 def main():
     # La classe Updater permet de lire en continu ce qu'il se passe sur le channel
@@ -36,10 +30,7 @@ def main():
 
     # On ajoute des gestionnaires de commandes
     # On donne a CommandHandler la commande textuelle et une fonction associée
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("site", site))
-    dp.add_handler(CommandHandler("youtube", youtube))
-    dp.add_handler(CommandHandler("linkedin", linkedin))
+    dp.add_handler(CommandHandler("cmd", fonction))
 
     # Pour gérer les autres messages qui ne sont pas des commandes
     dp.add_handler(MessageHandler(Filters.text, pas_compris))
